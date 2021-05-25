8220dbdba562a6696fedf56d03a8400a27ba6ba0
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-05-25 19:19:44 +0530

    Create Sender_CollectData.h

---
+++ BMS_Sender/Sender_CollectData.h
@@ -0,0 +1,19 @@
+/* *************************************************************************
+* File Name   :	Sender_CollectData.h
+* Description : Header file for BMS Sender Data Collection
+* ************************************************************************* */
+#ifndef SENDER_COLLECTDATA_H
+#define SENDER_COLLECTDATA_H
+
+/* ******************************* Defines ******************************* */
+#define MIN_TEMP 		-50.0
+#define MAX_TEMP		100.0
+#define ERROR_TEMP  150.0
+#define MIN_SOC  		0
+#define MAX_SOC  		100
+#define ERROR_SOC   255
+
+/* ***************************** Prototypes ***************************** */
+
+
+#endif
