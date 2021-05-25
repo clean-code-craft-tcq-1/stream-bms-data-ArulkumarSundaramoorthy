c67c75877166f3047ac27162e8b39a49495427e7
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-05-25 16:31:08 +0530

    Create Sender_CollectData.c

---
+++ BMS_Sender/Sender_CollectData.c
@@ -0,0 +1,8 @@
+/* ***********************************************************************************************************
+* File Name   :	Sender_CollectData.c
+* Author      : Arul Kumar Sundaramoorthy
+* Description : Battery Management System(BMS) Data Collection like Battery temperature and State of Charging
+* Functions	  :
+* *********************************************************************************************************** */
+
+/* ********************************************** Header Files ********************************************** */
