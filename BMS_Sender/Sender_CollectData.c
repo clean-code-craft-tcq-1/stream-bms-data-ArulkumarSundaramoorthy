0e4a8b5144d15671faf844963163c4ae6a2d30ac
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-05-26 15:42:14 +0530

    Update Sender_CollectData.c

--- BMS_Sender/Sender_CollectData.c
+++ BMS_Sender/Sender_CollectData.c
@@ -2,14 +2,14 @@
 * File Name   :	Sender_CollectData.c
 * Author      : Arul Kumar Sundaramoorthy
 * Description : Battery Management System(BMS) Data Collection like Battery temperature and State of Charging
-* Functions	  : isInRange, BmsSender_ReadTemperture
+* Functions   : isInRange, BmsSender_ReadTemperture
 * *********************************************************************************************************** */
 
 /* ********************************************************************************************************** */
 * Function Name : isInRange
 * Description   : Checks the current value within the maximum & minimum value
-* Arguments	    : current_value(float), min_value(float), max_value(float)
-* Returns		    : 1 (Value within range) or 0 (value out of range)
+* Arguments     : current_value(float), min_value(float), max_value(float)
+* Returns       : 1 (Value within range) or 0 (value out of range)
 /* ********************************************************************************************************** */
 int isInRange(float current_value, float min_value, float max_value)
 {
@@ -19,10 +19,10 @@ int isInRange(float current_value, float min_value, float max_value)
 /* **********************************************   Function   ********************************************** */
 * Function Name : BmsSender_ReadTemperture
 * Description   : Collect Battery Temperature Data from Sensor/DataSet
-* Arguments	    : -
-* Returns		    : Temperature value in float with one resolution
+* Arguments     : -
+* Returns       : Temperature value in float with one resolution
 /* **********************************************              ********************************************** */
-  float BmsSender_ReadTemperture()
+float BmsSender_ReadTemperture()
 {
   float Temperture;
   
