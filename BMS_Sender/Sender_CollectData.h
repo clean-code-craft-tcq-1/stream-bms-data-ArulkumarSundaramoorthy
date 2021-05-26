e30bd5d9e4fd9520abda44b2961ec15848f70187
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-05-26 15:38:29 +0530

    Update Sender_CollectData.h

--- BMS_Sender/Sender_CollectData.h
+++ BMS_Sender/Sender_CollectData.h
@@ -14,6 +14,6 @@
 #define ERROR_SOC   255
 
 /* ***************************** Prototypes ***************************** */
-
+int isInRange(float current_value, float min_value, float max_value);
 
 #endif
