57943dc26bd2ba943fc9a0b136d28124c6b923e1
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-05-26 15:40:29 +0530

    Update Sender_CollectData.h

--- BMS_Sender/Sender_CollectData.h
+++ BMS_Sender/Sender_CollectData.h
@@ -6,15 +6,16 @@
 #define SENDER_COLLECTDATA_H
 
 /* ******************************* Defines ******************************* */
-#define MIN_TEMP 		-50.0
-#define MAX_TEMP		100.0
+#define MIN_TEMP    -50.0
+#define MAX_TEMP    100.0
 #define ERROR_TEMP  150.0
-#define MIN_SOC  		0
-#define MAX_SOC  		100
+#define MIN_SOC     0
+#define MAX_SOC     100
 #define ERROR_SOC   255
 
 /* ***************************** Prototypes ***************************** */
 int isInRange(float current_value, float min_value, float max_value);
 float BmsSender_ReadTemperture();
+float BmsSender_ReadSOC();
 
 #endif
