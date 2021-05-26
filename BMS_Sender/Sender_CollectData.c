d796e3f1c2588df2e904fb90533c068c401a7645
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-05-26 16:12:10 +0530

    Update Sender_CollectData.c

--- BMS_Sender/Sender_CollectData.c
+++ BMS_Sender/Sender_CollectData.c
@@ -1,4 +1,4 @@
-/* ***********************************************************************************************************
+/* File Details ***********************************************************************************************
 * File Name   :	Sender_CollectData.c
 * Author      : Arul Kumar Sundaramoorthy
 * Description : Battery Management System(BMS) Data Collection like Battery temperature and State of Charging
@@ -9,7 +9,7 @@
 #include "Sender_CollectData.h"
 #include "stdio.h"
 
-/* ************************************************************************************************************
+/* Function Details *******************************************************************************************
 * Function Name : isInRange
 * Description   : Checks the current value within the maximum & minimum value
 * Arguments     : current_value(float), min_value(float), max_value(float)
@@ -20,7 +20,7 @@ int isInRange(float current_value, float min_value, float max_value)
   return((current_value >= min_value) && (current_value <= max_value));
 }
 
-/* ************************************************************************************************************
+/* Function Details *******************************************************************************************
 * Function Name : BmsSender_ReadTemperture
 * Description   : Collect Battery Temperature Data from Sensor/DataSet
 * Arguments     : -
@@ -28,7 +28,7 @@ int isInRange(float current_value, float min_value, float max_value)
 /* ********************************************************************************************************** */
 float BmsSender_ReadTemperture()
 {
-  float Temperture;
+  float Temperture = 0;
   
   if(isInRange(Temperture, MIN_TEMP, MAX_TEMP))
   {
@@ -38,7 +38,7 @@ float BmsSender_ReadTemperture()
   return ERROR_TEMP;
 }
 
-/* ************************************************************************************************************
+/* Function Details *******************************************************************************************
 * Function Name : BmsSender_ReadSOC
 * Description   : Collect Battery SOC Data from Sensor/DataSet
 * Arguments     : -
@@ -46,7 +46,7 @@ float BmsSender_ReadTemperture()
 /* ********************************************************************************************************** */
 float BmsSender_ReadSOC()
 {
-  float Soc;
+  float Soc = 0;
   
   if(isInRange(Soc, MIN_SOC, MAX_SOC))
   {
