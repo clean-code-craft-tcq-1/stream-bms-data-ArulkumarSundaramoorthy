b2d85fbfb3e41c80b45266a4fe37bfd07f240583
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-05-27 18:37:54 +0530

    Update Sender_SentType.c

--- BMS_Sender/Sender_SentType.c
+++ BMS_Sender/Sender_SentType.c
@@ -2,14 +2,13 @@
 * File Name   :	Sender_SentType.c
 * Author      : Arul Kumar Sundaramoorthy
 * Description : Battery Management System(BMS) Data sender type selection
-* Functions   : 
+* Functions   : BMS_SendData, sendToConsole
 * *********************************************************************************************************** */
 
 /* **************************************** Header Files **************************************************** */
 #include "Sender_CollectData.h"
 #include "Sender_SentType.h"
 #include "stdio.h"
-#include "stdlib.h"
 
 /* ***************************************** Variables ****************************************************** */
 SendStatus(*SendData[])(float, float) = {sendToConsole};
@@ -43,7 +42,7 @@ SendStatus BMS_SendData(SendType sendType)
 * *********************************************************************************************************** */
 SendStatus sendToConsole(float Temperature, float Soc) {
   
-  printf("Temperature:%0.1f,SOC:%0.1f\n", Temperature,Soc);
+  printf("Temperature:%3.1f,SOC:%3.1f\n", Temperature,Soc);
   
   return CONSOLE_SEND_PASS;
 }
