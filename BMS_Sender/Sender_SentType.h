1fcceebf1a6c4fc806d507fc362f6f1b770a9e0c
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-05-27 17:21:43 +0530

    Create Sender_SentType.h

---
+++ BMS_Sender/Sender_SentType.h
@@ -0,0 +1,18 @@
+/* *************************************************************************
+* File Name   :	Sender_SentType.h
+* Description : Header file for BMS Sender type selection
+* ************************************************************************* */
+
+/* ******************************* Defines ******************************* */
+typedef enum {
+  TO_CONSOLE,
+  MAX_TYPE
+} SendType;
+
+typedef enum {
+  SEND_FAIL,
+  CONSOLE_SEND_PASS
+} SendStatus;
+
+/* ***************************** Prototypes ***************************** */
+SendStatus sendToConsole(float Temperature, float Soc);
