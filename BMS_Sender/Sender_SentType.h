b54b17f27c86b1e83a40380adcbe6624b0cdfd29
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-05-27 17:26:40 +0530

    Update Sender_SentType.h

--- BMS_Sender/Sender_SentType.h
+++ BMS_Sender/Sender_SentType.h
@@ -14,5 +14,7 @@ typedef enum {
   CONSOLE_SEND_PASS
 } SendStatus;
 
+SendStatus(*SendData[])(float, float) = {sendToConsole};
+
 /* ***************************** Prototypes ***************************** */
 SendStatus sendToConsole(float Temperature, float Soc);
