2df6625a30da3b4559781f3cde2475d48172065d
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-05-27 16:16:43 +0530

    Create main.c

---
+++ BMS_Sender/main.c
@@ -0,0 +1,22 @@
+/* File Details ***********************************************************************************************
+* File Name   :	main.c
+* Author      : Arul Kumar Sundaramoorthy
+* Description : Battery Management System(BMS) Temperature & SOC Data Sends for every 1 Seconds
+* Functions   : main
+* *********************************************************************************************************** */
+
+/* **************************************** Header Files **************************************************** */
+#include "Sender_CollectData.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+/* Function Details *******************************************************************************************
+* Function Name : main
+* Description   : main function to transmit data for every 1 seconds
+* Arguments     : 
+* Returns       : 
+* *********************************************************************************************************** */
+int main() {
+    test_print();
+  return 0;
+}
