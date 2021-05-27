9377ad107b6ef17e4694c5c1ec749692a3dfce57
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-05-27 08:41:38 +0530

    Update test_Sender.cpp

--- test_Sender.cpp
+++ test_Sender.cpp
@@ -1,3 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+
+
+TEST_CASE("infers the breach according to limits LOW") {
+  REQUIRE(test_print());
+}
